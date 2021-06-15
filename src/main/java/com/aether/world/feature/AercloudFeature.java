31707280f7b470b311b9cb718b74bf36a89f5ec1
Crackers0106 <landonviles@gmail.com>
2021-06-14 20:36:08 -0400

    Classic Aerclouds

--- src/main/java/com/aether/world/feature/AercloudFeature.java
+++ src/main/java/com/aether/world/feature/AercloudFeature.java
@@ -6,11 +6,11 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.StructureWorldAccess;
-import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.util.FeatureContext;
 
 import java.util.Random;
-import java.util.function.Consumer;
+
 
 public class AercloudFeature extends Feature<AercloudConfig> {
     private static final Codec<AercloudConfig> CODEC = RecordCodecBuilder.create(instance -> instance.group(
@@ -26,48 +26,25 @@ public class AercloudFeature extends Feature<AercloudConfig> {
 
     @Override
     public boolean generate(StructureWorldAccess worldIn, ChunkGenerator generator, Random randIn, BlockPos posIn, AercloudConfig configIn) {
-        BlockPos origin = new BlockPos(posIn.getX(), (randIn.nextInt(configIn.isFlat() ? 4 : 64)) + configIn.getY(), posIn.getZ());
-        BlockPos.Mutable mut0 = new BlockPos.Mutable();
-        BlockPos.Mutable mut1 = new BlockPos.Mutable();
+        BlockPos origin = new BlockPos(pos.getX(), pos.getY(), pos.getZ());
+        BlockPos position = new BlockPos(origin.getX() + 8, origin.getY(), origin.getZ() + 8);
 
-        int x = 0, y = 0, z = 0;
-        int xTrend = randIn.nextInt() % 2 == 0 ? -1 : 1;
-        int zTrend = randIn.nextInt() % 2 == 0 ? -1 : 1;
-        int stride = randIn.nextInt(2) + 1;
-        boolean which = randIn.nextBoolean();
+        for (int amount = 0; amount < 16; ++amount) {
+            int xOffset = rand.nextInt(2);
+            int yOffset = (rand.nextBoolean() ? rand.nextInt(3) - 1 : 0);
+            int zOffset = rand.nextInt(2);
 
-        origin = origin.add(-64 * xTrend, 0, -64 * zTrend);
+            position = position.add(xOffset, yOffset, zOffset);
 
-        for (int i = 0; i < configIn.maxSegments(); ++i) {
-            int dX = randIn.nextInt(3);
-            int dY = randIn.nextInt(2) == 0 ? 0 : randIn.nextInt(3);
-            int dZ = randIn.nextInt(3);
+            for (int x = position.getX(); x < position.getX() + rand.nextInt(2) + 3; ++x) {
+                for (int y = position.getY(); y < position.getY() + rand.nextInt(1) + 2; ++y) {
+                    for (int z = position.getZ(); z < position.getZ() + rand.nextInt(2) + 3; ++z) {
+                        BlockPos newPosition = new BlockPos(x, y, z);
 
-            int xMax = randIn.nextInt(4) + 9;
-            int yMax = randIn.nextInt(1) + 2;
-            int zMax = randIn.nextInt(4) + 9;
-
-            int radius = randIn.nextInt(4) + 4;
-
-            x += dX + (xTrend * (which ? stride : 1));
-            y += dY - 1;
-            z += dZ + (zTrend * (which ? 1 : stride));
-
-            mut0.set(origin);
-            mut0.move(x, y, z);
-
-            for (int x0 = x - xMax; x0 < x + xMax; ++x0) {
-                for (int y0 = y; y0 < y + yMax; ++y0) {
-                    for (int z0 = z - zMax; z0 < z + zMax; ++z0) {
-                        mut1.set(origin);
-                        mut1.move(x0, y0, z0);
-
-                        try {
-                            if (worldIn.getBlockState(mut1).isAir() && mut1.isWithinDistance(mut0, radius)) {
-                                this.setBlockState(worldIn, mut1, configIn.getCloudState());
+                        if (reader.isAir(newPosition)) {
+                            if (Math.abs(x - position.getX()) + Math.abs(y - position.getY()) + Math.abs(z - position.getZ()) < 4 + rand.nextInt(2)) {
+                                this.setBlockState(reader, newPosition, state);
                             }
-                        } catch (NullPointerException e) {
-                            e.printStackTrace();
                         }
                     }
                 }
